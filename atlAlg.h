43e3f6630fa855f15548c7113284c81d72c85ccc
marybadalyan <marybadalyangdh@gmail.com>
2025-03-13 18:05:54 +0400

    Add search and find_end functions

--- atlAlg.h
+++ atlAlg.h
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <utility>
 
 
 namespace stlAlg{
-//the stl implementation uses find_if_not for all_of to minimize code 
-//repetition so we will implement it here 
+
     template <class InputIt,class Predicate>
     constexpr InputIt find_if_not(InputIt begin,InputIt end,Predicate p){
         for(;*begin!=end;++begin){
@@ -36,8 +36,7 @@ namespace stlAlg{
         return any_of(begin,end,p) != end;
     }
 
-    #include <utility>
-    #include <iostream>
+    
     template<class StartIterator,class Size,class Function>
     std::pair<StartIterator,Function> for_each_n(StartIterator start,Size n, Function f){
         for(Size i = 0;i < n;++i,++start){  //equallivent of comparing while (∗p!=0)
@@ -47,7 +46,6 @@ namespace stlAlg{
 
     }
 
-    
 
     template<class StartIterator,class EndIterator,class Function>
     Function for_each(StartIteratorstart,EndIterator end, Function f){
@@ -74,19 +72,88 @@ namespace stlAlg{
     constexpr bool none_of(InputIt begin,InputIt end,Predicate p){
         return find_if(begin,end,p) == end;
     }
-
-    #include <ranges>
-    #include <iostream>
-    template<class Range,class Function>
-    Function for_each(Range& r, Function f){
-        auto first = std::ranges::begin(r);
-        auto last = std::ranges::end(r);
-        for(;start!=end;++start){  //equallivent of comparing while (∗p!=0)
-            f(*start);
-        }
-        return f;    
+ 
     
+    template <class InputIt1,class InputIt2>
+    constexpr InputIt1 search(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2){
+        while(true){
+            InputIt1 start = begin1;
+            for(InputIt2 it = begin2;++it,++start){
+                if(it == end2){
+                    return start;
+                }
+                if(start == end1){
+                    return end1;
+                }
+                if(*it != *start)
+                {
+                    break;
+                }
+            }
+            ++begin1;
+        }
+    }
+
+    //implementing find_end with search
+    template <class InputIt1,class InputIt2>
+    constexpr InputIt1 find_end(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2){
+        if(begin2 == end2){
+            return end1;
+        }
+
+        InputIt1 end = end1;
+        while(true){
+            InputIt1 new_end = stlAlg::search(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2);
+            if(new_end == end){
+                break;
+            }
+            else{
+                end = new_end;
+                begin1 = end;
+                ++begin1;
+            }
+        }
+        return end;
+    }
+
+    template <class InputIt1,class InputIt2,class Pred>
+    constexpr InputIt1 search(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2,Pred p){
+        while(true){
+            InputIt1 start = begin1;
+            for(InputIt2 it = begin2;++it,++start){
+                if(it == end2){
+                    return start;
+                }
+                if(start == end1){
+                    return end1;
+                }
+                if(!p(*it,*start))
+                {
+                    break;
+                }
+            }
+            ++begin1;
+        }
+    }
+
+    template <class InputIt1,class InputIt2,class Pred>
+    constexpr InputIt1 find_end(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2,Pred p){
+        if(begin2 == end2){
+            return end1;
+        }
+
+        InputIt1 end = end1;
+        while(true){
+            InputIt1 new_end = stlAlg::search(InputIt1 begin1,InputIt1 end1,InputIt2 begin2,InputIt2 end2,p);
+            if(new_end == end){
+                break;
+            }
+            else{
+                end = new_end;
+                begin1 = end;
+                ++begin1;
+            }
+        }
+        return end;
     }
 }
-
-
